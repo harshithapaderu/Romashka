b89990f7c244cc938c0c55ce4e2057a240499a52
Preeti Chhabra <preetinarula412@gmail.com>
2019-05-18 22:40:55 +1000

    Add files via upload

---
+++ Beta/ROMASHKA_BETA_NEW/include/footer.php
@@ -0,0 +1,43 @@
+<!---->
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: HP-->
+<!-- * Date: 4/24/2019-->
+<!-- * Time: 11:18 AM-->
+<!-- */-->
+
+
+<footer id="myFooter">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-4 col-ms-4 col-xs-12 myCols">
+					<a href="index.html" class="footerlogo"><img src="images/footer_logo.png"/> &nbsp;<span>RUSSIAN LANGUAGE SCHOOL, <br>ROMAKSHA</span></a>
+					<div class="address">
+						<h6><img src="images/map.png"> 6 Lawson Street,Southport Gold coast, Queensland</h6>
+					</div>
+					
+                  </div>
+				<div class="col-sm-4 col-ms-4 col-xs-12 myCols">
+				<div class="social_links">
+						<h5>OUR SPONSORS</h5>
+						<a href="#" class="social_1"><img src="images/logo2s.jpg"/></a>
+						<a href="#" class="social_1"><img src="images/sponsor1.jpg"/></a>
+						
+					</div>
+                 </div>
+                               
+                <div class="col-sm-4 col-ms-4 col-xs-12 myCols1">
+					<div class="social_links">
+						<h5>GET IN TOUCH</h5>
+
+						<h6>Phone: 0468926494</h6>
+						<h6>Email: info@schoolromashka.com.au</h6>
+
+					  <a href="https://www.facebook.com/schoolromashka/" class="social_1"><img src="images/facebook.svg"/></a>
+                    <a href="#" class="social_1"><img src="images/linked_in.svg"/></a>
+                    <a href="#" class="social_1"><img src="images/insta.svg"/></a>
+                </div>
+                </div> 
+            </div>
+        </div>   
+	</footer>
