341c5cec480531b2c7627b5520f58c40913e2be6
Preeti Chhabra <preetinarula412@gmail.com>
2019-05-19 22:50:27 +0530

    Add files via upload

--- Beta/ROMASHKA_BETA_NEW/contactUs.php
+++ Beta/ROMASHKA_BETA_NEW/contactUs.php
@@ -1,56 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+ 
+	<title>Romashka Russian Language School</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	 <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+	
+	<link href="css/style.css" rel="stylesheet">
+  	
+<meta name="viewport" content="width=60%, initial-scale=1">
+<style>
 
+.contact_form {
+    padding: 15px 18px;
+    text-align: left;
+	width: 100%;
+    float: none;
+    background-color: white;
+    margin: auto;
+}
+.row {
+  width: 100%;
+      margin-left: 1px;
 
-    <title>Romashka Russian Language School</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+  margin-bottom: 2px;
+  padding: 10px 10px;
 
-    <link href="css/style.css" rel="stylesheet">
+}
 
-    <meta name="viewport" content="width=60%, initial-scale=1">
-    <style>
+.contact_form2 {
+    text-align: left;
+	width: 100%;
+	float: left;
+}
+.caption-container2 {
+  text-align: center;
+  background-color: #444;
+  padding: 4px 20px;
+  color: white;
+}
+.work_hd{
+	width: 100%;
+	float: left;
+	color: #000;
+	padding: 30px 15px 0px;
+}
 
-        .contact_form {
-            padding: 15px 18px;
-            text-align: left;
-            width: 100%;
-            float: none;
-            background-color: white;
-            margin: auto;
-        }
-        .row {
-            width: 100%;
-            margin-left: 1px;
-
-            margin-bottom: 2px;
-            padding: 10px 10px;
-
-        }
-
-        .contact_form2 {
-            text-align: left;
-            width: 100%;
-            float: left;
-        }
-        .caption-container2 {
-            text-align: center;
-            background-color: #444s;
-            padding: 4px 20px;
-            color: white;
-        }
-        .work_hd{
-            width: 100%;
-            float: left;
-            color: #000;
-            padding: 30px 15px 0px;
-        }
-
-    </style>
+</style>
 
 </head>
 <body >
@@ -59,61 +59,61 @@
 <?php include("include/header.php");?>
 
 <section class="contact_us">
-    <div class="container">
+<div class="container">
 
-        <h1 class="cont_hd">GET IN TOUCH</h1>
+    <h1 class="cont_hd">GET IN TOUCH</h1>
 
-        <div class="contact_form row" style="width:94%">
+    <div class="contact_form row" style="width:94%">
 
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <div class="contactform_box">
-                    <form method="post" class="contact_form2">
-                        <div class="form-group">
-                            <label class="control-label">Full Name</label>
-                            <input type="text" class="form-control" id="" name="" required />
-                        </div>
+  <div class="col-md-6 col-sm-6 col-xs-12">
+ <div class="contactform_box">
+  <form method="post" class="contact_form2">
+			<div class="form-group">
+				<label class="control-label">Full Name</label>
+				<input type="text" class="form-control" id="" name="" required />
+			</div>
+		
+	<div class="form-group">
+		<label for="" class="control-label">Businss Email (<a href="#">Privacy Policy</a>)</label>
+		<input type="text" class="form-control" name="" required />
+	</div>
+	<div class="form-group">
+		<label for="" class="control-label">Company</label>
+		<input type="text" class="form-control" name="" required />
+	</div>
+	<div class="form-group">
+		<label for="" class="control-label">Tell us about yourself</label>
+		<textarea type="text" class="form-control" name="" required ></textarea>
+	</div>
+	<div class="form-group">
+	<label for="" class="control-label">Would you like to subscribe to our Customer Experience Blog?</label>
+		<div class="radio">
+		  <label><input type="radio" name="optradio" checked> Yes</label>
+		</div>
+		<div class="radio">
+		  <label><input type="radio" name="optradio" checked> No</label>
+		</div>
+	</div>
+    <button class="btn-success btn submitbtn">Send Messege</button>
 
-                        <div class="form-group">
-                            <label for="" class="control-label">Business Email (<a href="#">Privacy Policy</a>)</label>
-                            <input type="text" class="form-control" name="" required />
-                        </div>
-                        <div class="form-group">
-                            <label for="" class="control-label">Company</label>
-                            <input type="text" class="form-control" name="" required />
-                        </div>
-                        <div class="form-group">
-                            <label for="" class="control-label">Tell us about yourself</label>
-                            <textarea type="text" class="form-control" name="" required ></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="" class="control-label">Would you like to subscribe to our Customer Experience Blog?</label>
-                            <div class="radio">
-                                <label><input type="radio" name="optradio" checked> Yes</label>
-                            </div>
-                            <div class="radio">
-                                <label><input type="radio" name="optradio" checked> No</label>
-                            </div>
-                        </div>
-                        <button class="btn-success btn submitbtn">Send Message</button>
+  </form>
+</div> 
+</div> 
+<div class="col-md-6 col-sm-6 col-xs-12">
 
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-
-                <div id="map">
-                    <div class="mapouter"><div class="gmap_canvas"><iframe width="100%"  id="gmap_canvas" src="https://maps.google.com/maps?q=cracker%20jack%20bulimba&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div>
-                        <style>.mapouter{text-align:right;height:419px;width:100%;}
-                            .gmap_canvas {overflow:hidden;background:none!important;height:419px;width:100%;padding-left: 100px;}
-                            #gmap_canvas{
-                                height: 100%;
-                                width: 100%;
-                            }
-                        </style></div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="map">
+<div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3523.7478472173443!2d153.41203671458956!3d-27.97100758268341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b910ff3231c50fd%3A0x200e7b3d7cbaf0c5!2s6+Lawson+St%2C+Southport+QLD+4215%2C+Australia!5e0!3m2!1sen!2sin!4v1557431281225!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe><a href="https://www.crocothemes.net"></a></div>
+<style>.mapouter{text-align:right;height:419px;width:100%;}
+.gmap_canvas {overflow:hidden;background:none!important;height:419px;width:100%;padding-left: 30px;}
+#gmap_canvas{
+    height: 100%;
+    width: 100%;
+}
+</style></div>
+</div>
+</div>
+</div>
+</div>
 
 </section>
 
