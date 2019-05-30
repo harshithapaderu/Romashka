0eca6a57e22d69186b4e5594c5607713732fdf50
Preeti Chhabra <preetinarula412@gmail.com>
2019-05-30 17:20:36 +0530

    Add files via upload

---
+++ Final/SOURCE CODE/newslist.php
@@ -0,0 +1,241 @@
+﻿<!---->
+<!--///**-->
+<!--// * Created by PhpStorm.-->
+<!--// * User: HP-->
+<!--// * Date: 4/26/2019-->
+<!--// * Time: 12:44 PM-->
+<!--// */-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Romashka Russian Language School</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+    <link href="css/style.css" rel="stylesheet">
+
+    <meta name="viewport" content="width=80%, initial-scale=1">
+    <style>
+
+        .column1 {
+            float: left;
+
+            width: 100%;
+
+            margin-bottom: 6px;
+            padding: 10px 8px;
+        }
+        .row1 {
+
+            width: 100%;
+            margin-bottom: 6px;
+
+            padding: 10px 10px;
+        }
+
+        .column {
+            width: 100%;
+            height: 500px;
+            margin-bottom: 1px;
+            padding: 5px 4px;
+        }
+        .row {
+            width: 100%;
+            margin-left: 1px;
+
+            margin-bottom: 2px;
+            padding: 10px 10px;
+
+        }
+
+        @media screen and (max-width: 650px) {
+            .column {
+                width: 100%;
+                display: block;
+            }
+        }
+
+        .card {
+
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            padding: 20px;
+        }
+
+        .container {
+            padding: 0 6px;
+        }
+
+        .container::after, .row::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
+
+        .title {
+            color: grey;
+        }
+
+        .button {
+            border: none;
+            outline: 0;
+            display: inline-block;
+            padding: 8px;
+            color: white;
+            background-color: #000;
+            text-align: center;
+            cursor: pointer;
+            width: 100%;
+        }
+        .right {
+            float: right;
+            width: 300px;
+            border: 3px solid #555555;
+            padding: 10px;
+        }
+        .button:hover {
+            background-color: #555;
+        }
+
+        .content1 {
+            max-width: 100%;
+            margin-left: 5%;
+            margin-right: 5%;
+
+
+
+            align: center
+        }
+    </style>
+
+</head>
+<body>
+
+<!-- Navigation -->
+<?php include("include/header.php");?>
+<section class="whyus">
+    <div class="content1" >
+        <div class="row1" >
+            <div class="column1">
+                <div class="card">
+                    <div class="row">
+
+                        <div class="container" style="width:100% ; text-align:center; ">
+                            <h2>News</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div class="column">
+                <div class="card">
+                                    <div class="container">
+                                            <div class="row" >
+
+ <div class="col-sm-5 col-ms-5 col-xs-12 myCols">
+                    <img src="images/health_news.jpg" alt="Jane" style="width:100%;height:240px;margin-bottom: 10px;">
+        </div>
+ <div class="col-sm-7 col-ms-7 col-xs-12 myCols" style="text-align:left">
+
+                     <h3>HEALTH NEWS</h3>
+                        <h5 class="title"><i class="fa fa-calendar" ></i> 08-05-2019
+        					
+      					</h5> 
+
+                        <p class="title1">Be You (previously Kids Matter) is a national initiative for educators and school communities, aimed at promoting and protecting positive mental health in children and young people.
+Our vision at Bull Creek Primary School is that our learning community is positive, inclusive and resilient, and where every child, young person, staff member and family can achieve their best possible mental health....
+</p>
+                        <p><button ><a href="news1detail.php">Read More</a></button></p>
+                    </div>
+                            </div>
+                            </div>
+
+                </div>
+            </div>
+ <div class="column">
+                <div class="card">
+                                    <div class="container">
+                                            <div class="row" >
+
+ <div class="col-sm-5 col-ms-5 col-xs-12 myCols">
+                    <img src="images/kidswim.jpg" alt="Jane" style="width:100%;height:240px;margin-bottom: 10px;">
+        </div>
+ <div class="col-sm-7 col-ms-7 col-xs-12 myCols" style="text-align:left">
+
+                     <h3> Devonshire’s swimmers</h3>
+                        <h5 class="title"><i class="fa fa-calendar" ></i> 08-05-2019
+        					
+      					</h5> 
+
+                        <p class="title1">Last Friday evening 20 swimmers represented Devonshire at the borough school’s swimming gala. Children from Year 4 upwards competed against 8 other schools. The children were fantastic ambassadors for the school and we were all very proud of them and their tremendous efforts.A special mention must go to our girl’s medley relay...</p>
+                        <p><button ><a href="news1detail.php">Read More</a></button></p>
+                    </div>
+                            </div>
+                            </div>
+
+                </div>
+            </div>
+ 
+		<div class="column">
+                <div class="card">
+                                    <div class="container">
+                                            <div class="row" >
+
+ <div class="col-sm-5 col-ms-5 col-xs-12 myCols">
+                    <img src="images/event6.jpg" alt="Jane" style="width:100%;height:240px;margin-bottom: 10px;">
+        </div>
+ <div class="col-sm-7 col-ms-7 col-xs-12 myCols" style="text-align:left">
+
+                     <h3>ANNUAL ALUMNI MEET 2018–19</h3>
+                        <h5 class="title"><i class="fa fa-calendar" ></i> 08-05-2019
+        					
+      					</h5> 
+
+                        <p class="title1">The truth is homecoming never gets old. On the 17th of November, our dear school held their Annual Alumni Meet. It was amazing to see all the ex horizonites come back once again to the home that they shared for 13 years of their lives. There were students from the 1982 batch to the 2018 batch. The age gap between the two was definitely there but we shared the same feelings at heart....
+</p>
+                        <p><button ><a href="news1detail.php">Read More</a></button></p>
+                    </div>
+                            </div>
+                            </div>
+
+                </div>
+            </div>
+ <div class="column">
+                <div class="card">
+                                    <div class="container">
+                                            <div class="row" >
+
+ <div class="col-sm-5 col-ms-5 col-xs-12 myCols">
+                    <img src="images/event1.jpg" alt="Jane" style="width:100%;height:240px;margin-bottom: 10px;">
+        </div>
+ <div class="col-sm-7 col-ms-7 col-xs-12 myCols" style="text-align:left">
+
+                     <h3>PTG’s Back to School Picnic</h3>
+                        <h5 class="title"><i class="fa fa-calendar" ></i> 08-05-2019
+        					
+      					</h5> 
+
+                        <p class="title1">The PTG’s Back to School Picnic is a great way for new and returning families, staff, and alumni to start off the school year.  Food, fun and games for all.  A wonderful time to meet the teachers, welcome new families, and see old friendsIn the winter trimester, our attention turns to The All School Production, an extravaganza of singing, dancing making costumes, working on lights, sound, hair, and acting..</p>
+                        <p><button ><a href="news1detail.php">Read More</a></button></p>
+                    </div>
+                            </div>
+                            </div>
+
+                </div>
+            </div>
+ 
+    </div>
+
+</section>
+
+
+<?php include("include/footer.php");?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+</body>
+</html>
